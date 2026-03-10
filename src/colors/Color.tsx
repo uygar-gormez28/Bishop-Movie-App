f36c99b7b227ddf8665a512f7f049f7e03535e3c
Uygar <121864027+uygar-gormez28@users.noreply.github.com>
2026-03-10 03:42:26 +0300

    colorlar için them dşye özel bir file açıp export ettim

---
+++ src/colors/Color.tsx
@@ -0,0 +1,9 @@
+const colors = {
+  backgroundColor: "#0f172a",
+  textColor: "#e5e7eb",
+  borderColor: "#1f2937",
+  activeColor: "#60a5fa",
+  inActiveColor: "#94a3b8",
+};
+
+export default colors;
--- src/theme/Color.tsx
+++
@@ -1,3 +0,0 @@
-const color = {};
-
-export default color;
